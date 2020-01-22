9e1b2ed74a1384a0d427f39931aa1d5d635020bc
Mohammad-Quanit <muhammadquanit@gmail.com>
2020-01-22 17:43:01 +0500

    Added: user auth module and restructured code

--- streams.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-let myReadStream = fs.createReadStream(`${__dirname}/data-file/data.txt`, 'utf8'); 
-
-myReadStream.on('data', chunk => {
-  console.log(`New chunk received..........`);
-  console.log(chunk);
-})
