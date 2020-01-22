77182615ec99aff600f1252e47a624a2d69ba8d9
Mohammad-Quanit <muhammadquanit@gmail.com>
2020-01-22 19:04:29 +0500

    Added: user auth module and restructured code

--- server.js
+++ server.js
@@ -38,8 +38,6 @@ app.use('./app/upload', express.static(__dirname + '/app/upload/'));
 
 let introHtml = `
   <h1>Welcome to Demo CRUD Api's</h1>
-  <h3>Go to <a href="https://crud-demo-api.herokuapp.com/exercises">https://crud-demo-api.herokuapp.com/exercises</a> for any demo Todo list</h3>
-  <h3>Go to <a href="https://crud-demo-api.herokuapp.com/users">https://crud-demo-api.herokuapp.com/users</a> for any demo User list</h3>
 `;
 
 app.get("/", (req, res) => res.send(introHtml));
